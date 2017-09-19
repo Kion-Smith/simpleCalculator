e2a26e468ac0526b06dade6ddc283095b8d91a8c
Kion Smith <kion_smith@hotmail.com>
2017-09-19 18:52:58 -0500

    Added functionality to the buttons

--- Simple Calculator/src/Calculator.java
+++ Simple Calculator/src/Calculator.java
@@ -1,18 +1,21 @@
 import java.awt.BorderLayout;
+import java.awt.*;
+import java.awt.event.*;
+
+
 
 import javax.swing.*;
 
 
-public class Calculator extends JFrame
+public class Calculator extends JFrame implements ActionListener
 {
 	JPanel panel1;// for holding the user input
 	JPanel panel2;// for holding the buttons that manipulate input
-	
 	JPanel panel3;// the result labels
 	
 	JLabel num1Label,num2Label,resLabel;// the various labels
 	
-	JButton addButton,subButton,multButton;//all the buttons
+	JButton addButton,subButton,multButton,divButton;//all the buttons
 	
 	JTextField num1Field,num2Field,resField;// all the fields to write in
 	public Calculator()
@@ -36,6 +39,7 @@ public class Calculator extends JFrame
 		addButton = new JButton("Add");
 		subButton = new JButton("Sub");
 		multButton = new JButton("Mult");
+		divButton = new JButton("Div");
 		
 		
 		// add the components to the 1st panel
@@ -47,6 +51,7 @@ public class Calculator extends JFrame
 		panel2.add(addButton);
 		panel2.add(subButton);
 		panel2.add(multButton);
+		panel2.add(divButton);
 		// add the components to the 3rd panel
 		panel3.add(resLabel);
 		panel3.add(resField);
@@ -56,5 +61,42 @@ public class Calculator extends JFrame
 		add(panel2,BorderLayout.CENTER);//put the contents of panel 2 in the middle
 		add(panel3,BorderLayout.SOUTH);//put the contents of panel 3 at the bottom
 		
+		addButton.addActionListener(this);
+		subButton.addActionListener(this);
+		multButton.addActionListener(this);
+		divButton.addActionListener(this);
+		
 	}
+	
+	public void actionPerformed(ActionEvent e) 
+	{
+		//getting the numbers from the text field
+			double num1 = Double.parseDouble(num1Field.getText());
+			double num2 = Double.parseDouble(num2Field.getText());		
+			
+		if(e.getSource() == addButton)// if the button pressed is equal to add then add
+		{
+			resField.setText((num1 + num2)+"");
+		}
+		else if(e.getSource() == subButton)// if the button pressed is equal to sub then sub
+		{
+			resField.setText((num1-num2)+"");
+		}
+		else if(e.getSource() == multButton)// if the button pressed is equal to mult then mult
+		{
+			resField.setText((num1*num2)+"");
+		}
+		else if(e.getSource() == divButton)// if the button pressed is equal to div then div
+		{
+			if(num2 != 0)// make sure num2 is not zero so no divide by zero error
+			{
+				resField.setText((num1/num2)+"");
+			}
+			else
+			{
+				resField.setText("Cannot divide by 0");
+			}
+		}
+	}
+
 }
